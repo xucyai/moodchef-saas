e06de59b9257a611dccfab5faeb57a415e168418
xucyai <lovely36012@gmail.com>
2025-06-20 04:05:47 +0800

    Add files via upload

---
+++ welcome.tsx
@@ -0,0 +1,8 @@
+export default function Welcome() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-3xl font-semibold">已開通 Premium 功能 🎉</h1>
+      <p className="mt-4">你已成功進入專屬分析頁面，Notion 寫入與 LINE 推播已啟用！</p>
+    </div>
+  );
+}
